213803d46ca2ce8df93ab6b9cce762d497c38095
R Z <91019650+RZ781@users.noreply.github.com>
2025-12-02 09:12:32 +0000

    Added real tabs

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,35 @@ class MainWindow: public Gtk::Window {
 	MainWindow();
 	private:
 	Gtk::Notebook notebook;
-	Gtk::Frame tab1;
-	Gtk::Frame tab2;
+	Gtk::Box code_tab;
+	Gtk::Box vm_tab;
+	Gtk::Box variable_tab;
+	Gtk::TextView source_code_view;
+	Gtk::Frame vm_registers;
+	Gtk::Frame vm_code;
+	Gtk::Frame variable_list;
+	Gtk::Grid variables;
 };
 
-MainWindow::MainWindow(): notebook(), tab1("Tab 1"), tab2("Tab 2") {
-	notebook.append_page(tab1, "Tab 1");
-	notebook.append_page(tab2, "Tab 2");
+MainWindow::MainWindow():
+	notebook(),
+	code_tab(),
+	vm_tab(),
+	variable_tab(),
+	source_code_view(),
+	vm_registers(),
+	vm_code(),
+	variable_list(),
+	variables()
+{
+	notebook.append_page(code_tab, "Code");
+	notebook.append_page(vm_tab, "Virtual Machine");
+	notebook.append_page(variable_tab, "Variables");
+	code_tab.append(source_code_view);
+	vm_tab.append(vm_registers);
+	vm_tab.append(vm_code);
+	variable_tab.append(variable_list);
+	variable_list.set_child(variables);
 	set_title("Virtual machine");
 	set_default_size(800, 600);
 	set_child(notebook);
