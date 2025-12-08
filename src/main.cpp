f17db29d315f04858a84aaf2f385f290d2e94c07
R Z <91019650+RZ781@users.noreply.github.com>
2025-12-08 11:41:13 +0000

    Finished working code tab

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,18 @@ class MainWindow: public Gtk::Window {
 	MainWindow();
 	private:
 	Gtk::Notebook notebook;
-	Gtk::Box code_tab;
-	Gtk::Box vm_tab;
-	Gtk::Box variable_tab;
+	Gtk::Paned code_tab;
+	Gtk::Box compilation_options;
+	Gtk::CheckButton c_button;
+	Gtk::CheckButton cpp_button;
+	Gtk::CheckButton asm_button;
+	Gtk::CheckButton standard_library_box;
+	Gtk::Button compile_button;
 	Gtk::TextView source_code_view;
+	Gtk::Box vm_tab;
 	Gtk::Frame vm_registers;
 	Gtk::Frame vm_code;
+	Gtk::Box variable_tab;
 	Gtk::Frame variable_list;
 	Gtk::Grid variables;
 };
@@ -18,22 +24,42 @@ class MainWindow: public Gtk::Window {
 MainWindow::MainWindow():
 	notebook(),
 	code_tab(),
-	vm_tab(),
-	variable_tab(),
+	compilation_options(Gtk::Orientation::VERTICAL),
+	c_button("C"),
+	cpp_button("C++"),
+	asm_button("Assembly"),
+	standard_library_box("Include standard library"),
+	compile_button("Compile"),
 	source_code_view(),
+	vm_tab(),
 	vm_registers(),
 	vm_code(),
+	variable_tab(),
 	variable_list(),
 	variables()
 {
+	// set up code tab
 	notebook.append_page(code_tab, "Code");
+	code_tab.set_start_child(source_code_view);
+	code_tab.set_end_child(compilation_options);
+	compilation_options.append(c_button);
+	compilation_options.append(cpp_button);
+	compilation_options.append(asm_button);
+	compilation_options.append(standard_library_box);
+	compilation_options.append(compile_button);
+	compilation_options.set_homogeneous();
+	cpp_button.set_group(c_button);
+	asm_button.set_group(c_button);
+	compilation_options.set_margin(50);
+	// set up vm tab
 	notebook.append_page(vm_tab, "Virtual Machine");
-	notebook.append_page(variable_tab, "Variables");
-	code_tab.append(source_code_view);
 	vm_tab.append(vm_registers);
 	vm_tab.append(vm_code);
+	// set up variables tab
+	notebook.append_page(variable_tab, "Variables");
 	variable_tab.append(variable_list);
 	variable_list.set_child(variables);
+	// set up window
 	set_title("Virtual machine");
 	set_default_size(800, 600);
 	set_child(notebook);
