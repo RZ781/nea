2e37f2cf624f94639562e9986769d32a0a8876d3
R Z <91019650+RZ781@users.noreply.github.com>
2026-02-04 22:22:19 +0000

    Gotten arm libc stuff to compile

--- test.c
+++ test.c
@@ -1,37 +1,5 @@
-#include <stdint.h>
-#include <linux/unistd.h>
+#include <stdio.h>
 
-uint32_t syscall(uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t n);
-
-int length(const char* string) {
-	int i = 0;
-	while (string[i] != 0)
-		i++;
-	return i;
-}
-
-void exit(int code) {
-	syscall(code, 0, 0, __NR_exit);
-}
-
-void print(const char* string) {
-	syscall(1, (uint32_t) string, length(string), __NR_write);
-}
-
-void print_int(int x) {
-	char string[10];
-	int i = 0;
-	while (x) {
-		string[i] = x % 10 + '0';
-		x /= 10;
-		i++;
-	}
-	string[i] = 0;
-	print(string);
-}
-
-__attribute__((naked))
-void _start() {
-	print_int(67);
-	exit(0);
+int main() {
+	printf("hello world\n");
 }
